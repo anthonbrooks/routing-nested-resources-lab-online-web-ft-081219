6b1f1d2084d0dccdeee37047a7f1af1ca17c3686
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-10-30 18:38:53 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,6 +15,7 @@ class SongsController < ApplicationController
   def show
     if params[:artist_id]
       @artist = Artist.find(params[:artist_id])
+      @song = 
       if @artist.nil?
         redirect_to artist_path, alert: 'Song not found'
       else 
